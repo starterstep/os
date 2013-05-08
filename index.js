10e50003b73d09687803fcdb8b287453517b666b
btknorr <btknorr@gmail.com>
2013-05-07 20:11:28 -0600

    android, ios

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function os() {
   if (/mac/i.test(ua)) return 'mac';
   if (/win/i.test(ua)) return 'windows';
   if (/linux/i.test(ua)) return 'linux';
+  if (/iphone/i.test(ua)) return 'ios';
+  if (/ipad/i.test(ua)) return 'ios';
+  if (/android/i.test(ua)) return 'android';
 }
